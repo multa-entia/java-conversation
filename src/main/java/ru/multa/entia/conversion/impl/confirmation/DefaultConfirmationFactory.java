5fa3c80b7e8a32e351fb84a220e22920e98fe769
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-10-04 22:38:06 +0300

    DefaultConfirmationFactory is done

--- src/main/java/ru/multa/entia/conversion/impl/confirmation/DefaultConfirmationFactory.java
+++ src/main/java/ru/multa/entia/conversion/impl/confirmation/DefaultConfirmationFactory.java
@@ -1,26 +1,83 @@
 package ru.multa.entia.conversion.impl.confirmation;
 
 import ru.multa.entia.conversion.api.SimpleFactory;
+import ru.multa.entia.conversion.api.address.Address;
 import ru.multa.entia.conversion.api.confirmation.Confirmation;
 import ru.multa.entia.conversion.api.message.Message;
 import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.api.seed.Seed;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
+import ru.multa.entia.results.impl.seed.DefaultSeedBuilder;
 
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.function.Function;
 
 public class DefaultConfirmationFactory implements SimpleFactory<Message, Confirmation> {
-    public static final String KEY__FROM_DECORATOR = "default-confirmation=factory.from-decorator";
-    public static final String KEY__CODE = "default-confirmation=factory.code";
-    public static final String KEY__ARGS = "default-confirmation=factory.args";
+    public static final String KEY__FROM_DECORATOR = "default-confirmation-factory.from-decorator";
+    public static final String KEY__CODE = "default-confirmation-factory.code";
+    public static final String KEY__ARGS = "default-confirmation-factory.args";
+
+    public static final String CODE__MESSAGE_NULL = "conversation.factory.confirmation.message-null";
+    public static final String CODE__FIELD_NULL = "conversation.factory.confirmation.id-null";
+
+    public static final String ALIAS__ID = " id";
+    public static final String ALIAS__CONVERSATION = " conversation";
+    public static final String ALIAS__FROM = " from";
+    public static final String ALIAS__TO = " to";
+
 
     @Override
     public Result<Confirmation> create(final Message message, final Object... args) {
-        return null;
+        Seed seed = checkForNull(message);
+        if (seed == null){
+            seed = checkFieldsForNull(message);
+        }
+
+        return seed == null
+                ? DefaultResultBuilder.<Confirmation>ok(createConfirmation(message, args))
+                : DefaultResultBuilder.<Confirmation>fail(seed);
+    }
+
+    private Seed checkForNull(final Message message){
+        return message == null
+                ? new DefaultSeedBuilder<Confirmation>().code(CODE__MESSAGE_NULL).build()
+                : null;
+    }
+
+    private Seed checkFieldsForNull(final Message message){
+        StringBuilder nullFields = new StringBuilder();
+        if (message.id() == null){
+            nullFields.append(ALIAS__ID);
+        }
+        if (message.conversation() == null){
+            nullFields.append(ALIAS__CONVERSATION);
+        }
+        if (message.from() == null){
+            nullFields.append(ALIAS__FROM);
+        }
+        if (message.to() == null){
+            nullFields.append(ALIAS__TO);
+        }
+
+        return nullFields.isEmpty()
+                ? null
+                : new DefaultSeedBuilder<Confirmation>().code(CODE__FIELD_NULL).addLastArgs(nullFields.toString()).build();
+    }
+
+    private Confirmation createConfirmation(final Message message, final Object[] args){
+        return new DefaultConfirmation(
+                message.id(),
+                message.conversation(),
+                new FromDecoratorGetter().apply(args).decorate(message.to()),
+                message.from(),
+                new CodeGetter().apply(args),
+                new ArgsGetter().apply(args)
+        );
     }
 
     public interface Decorator {
-        String decorate(String value);
+        Address decorate(Address value);
     }
 
     public static class FromDecoratorGetter implements Function<Object[], Decorator> {
--- src/test/java/ru/multa/entia/conversion/impl/confirmation/DefaultConfirmationFactoryTest.java
+++ src/test/java/ru/multa/entia/conversion/impl/confirmation/DefaultConfirmationFactoryTest.java
@@ -1,7 +1,15 @@
 package ru.multa.entia.conversion.impl.confirmation;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.multa.entia.conversion.api.address.Address;
+import ru.multa.entia.conversion.api.confirmation.Confirmation;
+import ru.multa.entia.conversion.api.message.Message;
 import ru.multa.entia.fakers.impl.Faker;
+import ru.multa.entia.results.api.result.Result;
+
+import java.util.Arrays;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,7 +21,7 @@ class DefaultConfirmationFactoryTest {
                 = new DefaultConfirmationFactory.FromDecoratorGetter().apply(null);
 
         String expected = Faker.str_().random();
-        assertThat(decorator.decorate(expected)).isEqualTo(expected);
+        assertThat(decorator.decorate(createTestAddress(expected)).value()).isEqualTo(expected);
     }
 
     @Test
@@ -22,7 +30,7 @@ class DefaultConfirmationFactoryTest {
                 = new DefaultConfirmationFactory.FromDecoratorGetter().apply(new Object[]{});
 
         String expected = Faker.str_().random();
-        assertThat(decorator.decorate(expected)).isEqualTo(expected);
+        assertThat(decorator.decorate(createTestAddress(expected)).value()).isEqualTo(expected);
     }
 
     @Test
@@ -32,7 +40,7 @@ class DefaultConfirmationFactoryTest {
                 = new DefaultConfirmationFactory.FromDecoratorGetter().apply(args);
 
         String expected = Faker.str_().random();
-        assertThat(decorator.decorate(expected)).isEqualTo(expected);
+        assertThat(decorator.decorate(createTestAddress(expected)).value()).isEqualTo(expected);
     }
 
     @Test
@@ -43,7 +51,7 @@ class DefaultConfirmationFactoryTest {
                 = new DefaultConfirmationFactory.FromDecoratorGetter().apply(args);
 
         String expected = Faker.str_().random();
-        assertThat(decorator.decorate(expected)).isEqualTo(expected);
+        assertThat(decorator.decorate(createTestAddress(expected)).value()).isEqualTo(expected);
     }
 
     @Test
@@ -110,5 +118,215 @@ class DefaultConfirmationFactoryTest {
         Object[] args = new DefaultConfirmationFactory.ArgsGetter().apply(inputArgs);
 
         assertThat(args).isEqualTo(expectedArgs);
+    }
+
+    @Test
+    void shouldCheckCreation_ifMessageNull() {
+        Result<Confirmation> result = new DefaultConfirmationFactory().create(null);
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(DefaultConfirmationFactory.CODE__MESSAGE_NULL);
+    }
+
+    @Test
+    void shouldCheckCreation_ifIdMessageNull() {
+        Result<Confirmation> result = new DefaultConfirmationFactory().create(createTestMessage(
+                null,
+                Faker.uuid_().random(),
+                createTestAddress(Faker.str_().random()),
+                createTestAddress(Faker.str_().random())
+        ));
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(DefaultConfirmationFactory.CODE__FIELD_NULL);
+        Object[] args = result.seed().args();
+        assertThat(args).hasSize(1);
+        assertThat(args[0]).isEqualTo(DefaultConfirmationFactory.ALIAS__ID);
+    }
+
+    @Test
+    void shouldCheckCreation_ifConversationMessageNull() {
+        Result<Confirmation> result = new DefaultConfirmationFactory().create(createTestMessage(
+                Faker.uuid_().random(),
+                null,
+                createTestAddress(Faker.str_().random()),
+                createTestAddress(Faker.str_().random())
+        ));
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(DefaultConfirmationFactory.CODE__FIELD_NULL);
+        Object[] args = result.seed().args();
+        assertThat(args).hasSize(1);
+        assertThat(args[0]).isEqualTo(DefaultConfirmationFactory.ALIAS__CONVERSATION);
+    }
+
+    @Test
+    void shouldCheckCreation_ifFromMessageNull() {
+        Result<Confirmation> result = new DefaultConfirmationFactory().create(createTestMessage(
+                Faker.uuid_().random(),
+                Faker.uuid_().random(),
+                null,
+                createTestAddress(Faker.str_().random())
+        ));
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(DefaultConfirmationFactory.CODE__FIELD_NULL);
+        Object[] args = result.seed().args();
+        assertThat(args).hasSize(1);
+        assertThat(args[0]).isEqualTo(DefaultConfirmationFactory.ALIAS__FROM);
+    }
+
+    @Test
+    void shouldCheckCreation_ifToMessageNull() {
+        Result<Confirmation> result = new DefaultConfirmationFactory().create(createTestMessage(
+                Faker.uuid_().random(),
+                Faker.uuid_().random(),
+                createTestAddress(Faker.str_().random()),
+                null
+        ));
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(DefaultConfirmationFactory.CODE__FIELD_NULL);
+        Object[] args = result.seed().args();
+        assertThat(args).hasSize(1);
+        assertThat(args[0]).isEqualTo(DefaultConfirmationFactory.ALIAS__TO);
+    }
+
+    @Test
+    void shouldCheckCreation_ifAllMessageFieldNull() {
+        Result<Confirmation> result = new DefaultConfirmationFactory().create(createTestMessage(
+                null,
+                null,
+                null,
+                null
+        ));
+
+        String expectedArg = DefaultConfirmationFactory.ALIAS__ID +
+                DefaultConfirmationFactory.ALIAS__CONVERSATION +
+                DefaultConfirmationFactory.ALIAS__FROM +
+                DefaultConfirmationFactory.ALIAS__TO;
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(DefaultConfirmationFactory.CODE__FIELD_NULL);
+        Object[] args = result.seed().args();
+        assertThat(args).hasSize(1);
+        assertThat(args[0]).isEqualTo(expectedArg);
+    }
+
+    @Test
+    void shouldCheckCreation_withDecorator() {
+        DefaultConfirmationFactory.Decorator d = (s) -> {
+            return createTestAddress(String.format("__%s__", s.value()));
+        };
+        Object[] args = {null, DefaultConfirmationFactory.KEY__FROM_DECORATOR, d};
+
+        UUID initId = Faker.uuid_().random();
+        UUID initConversation = Faker.uuid_().random();
+        Address initFrom = createTestAddress(Faker.str_().random());
+        Address initTo = createTestAddress(Faker.str_().random());
+        Result<Confirmation> result = new DefaultConfirmationFactory().create(
+                createTestMessage(initId, initConversation, initFrom, initTo),
+                args
+        );
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.seed()).isNull();
+
+        Confirmation confirmation = result.value();
+        assertThat(confirmation.id()).isEqualTo(initId);
+        assertThat(confirmation.conversation()).isEqualTo(initConversation);
+        assertThat(confirmation.from().value()).isEqualTo(d.decorate(initTo).value());
+        assertThat(confirmation.to().value()).isEqualTo(initFrom.value());
+    }
+
+    @Test
+    void shouldCheckCreation_withCodeAndArgs() {
+        String expectedCode = Faker.str_().random();
+        String arg0 = Faker.str_().random();
+        String arg1 = Faker.str_().random();
+        Object[] args = {
+                null,
+                DefaultConfirmationFactory.KEY__CODE,
+                expectedCode,
+                DefaultConfirmationFactory.KEY__ARGS,
+                arg0,
+                arg1
+        };
+
+        Object[] expectedArgs = {arg0, arg1};
+
+        UUID initId = Faker.uuid_().random();
+        UUID initConversation = Faker.uuid_().random();
+        Address initFrom = createTestAddress(Faker.str_().random());
+        Address initTo = createTestAddress(Faker.str_().random());
+        Result<Confirmation> result = new DefaultConfirmationFactory().create(
+                createTestMessage(initId, initConversation, initFrom, initTo),
+                args
+        );
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.seed()).isNull();
+
+        Confirmation confirmation = result.value();
+        assertThat(confirmation.id()).isEqualTo(initId);
+        assertThat(confirmation.conversation()).isEqualTo(initConversation);
+        assertThat(confirmation.from().value()).isEqualTo(initTo.value());
+        assertThat(confirmation.to().value()).isEqualTo(initFrom.value());
+        assertThat(confirmation.code()).isEqualTo(expectedCode);
+        assertThat(Arrays.equals(confirmation.args(), expectedArgs)).isTrue();
+    }
+
+    @Test
+    void shouldCheckCreation() {
+        UUID initId = Faker.uuid_().random();
+        UUID initConversation = Faker.uuid_().random();
+        Address initFrom = createTestAddress(Faker.str_().random());
+        Address initTo = createTestAddress(Faker.str_().random());
+        Result<Confirmation> result = new DefaultConfirmationFactory().create(
+                createTestMessage(initId, initConversation, initFrom, initTo)
+        );
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.seed()).isNull();
+
+        Confirmation confirmation = result.value();
+        assertThat(confirmation.id()).isEqualTo(initId);
+        assertThat(confirmation.conversation()).isEqualTo(initConversation);
+        assertThat(confirmation.from().value()).isEqualTo(initTo.value());
+        assertThat(confirmation.to().value()).isEqualTo(initFrom.value());
+        assertThat(confirmation.code()).isNull();
+        assertThat(confirmation.args()).isEmpty();
+    }
+
+    private Address createTestAddress(String value){
+        Address address = Mockito.mock(Address.class);
+        Mockito
+                .when(address.value())
+                .thenReturn(value);
+
+        return address;
+    }
+
+    private Message createTestMessage(UUID id, UUID conversation, Address from, Address to){
+        Message message = Mockito.mock(Message.class);
+        Mockito
+                .when(message.id())
+                .thenReturn(id);
+        Mockito
+                .when(message.conversation())
+                .thenReturn(conversation);
+        Mockito
+                .when(message.from())
+                .thenReturn(from);
+        Mockito
+                .when(message.to())
+                .thenReturn(to);
+
+        return message;
     }
 }
