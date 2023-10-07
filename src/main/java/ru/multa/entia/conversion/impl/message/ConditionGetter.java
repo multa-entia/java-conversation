f3a3f17da569b0bbc0860ea14839291e07bec417
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-10-07 17:20:21 +0300

    DefaultGetter is done

---
+++ src/main/java/ru/multa/entia/conversion/impl/message/ConditionGetter.java
@@ -0,0 +1,14 @@
+package ru.multa.entia.conversion.impl.message;
+
+import ru.multa.entia.results.api.result.Result;
+
+import java.util.function.Function;
+
+// TODO: 05.10.2023 for address
+class ConditionGetter<T> implements Function<Object[], Result<T>> {
+
+    @Override
+    public Result<T> apply(Object[] objects) {
+        return null;
+    }
+}
---
+++ src/test/java/ru/multa/entia/conversion/impl/message/DefaultGetterTest.java
@@ -0,0 +1,76 @@
+package ru.multa.entia.conversion.impl.message;
+
+import org.junit.jupiter.api.Test;
+import ru.multa.entia.fakers.impl.Faker;
+import ru.multa.entia.results.api.result.Result;
+
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DefaultGetterTest {
+    private static final UUID DEFAULT_VALUE = Faker.uuid_().random();
+    private static final DefaultMessageFactory.Keys KEY = DefaultMessageFactory.Keys.ID;
+    private static final Supplier<UUID> DEFAUT_SUPPLIER = () -> {return DEFAULT_VALUE;};
+
+    @Test
+    void shouldCheckGetting_ifArgsIsNull() {
+        Result<UUID> result = new DefaultGetter<UUID>(KEY, DEFAUT_SUPPLIER).apply(null);
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.value()).isEqualTo(DEFAULT_VALUE);
+        assertThat(result.seed()).isNull();
+    }
+
+    @Test
+    void shouldCheckGetting_ifArgsDoesNotContainKey() {
+        Result<UUID> result = new DefaultGetter<UUID>(KEY, DEFAUT_SUPPLIER).apply(new Object[0]);
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.value()).isEqualTo(DEFAULT_VALUE);
+        assertThat(result.seed()).isNull();
+    }
+
+    @Test
+    void shouldCheckGetting_ifArgsDoesNotContainValue() {
+        Object[] args = {
+                null,
+                KEY
+        };
+        Result<UUID> result = new DefaultGetter<UUID>(KEY, DEFAUT_SUPPLIER).apply(args);
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.value()).isEqualTo(DEFAULT_VALUE);
+        assertThat(result.seed()).isNull();
+    }
+
+    @Test
+    void shouldCheckGetting_ifArgsContainsBadValue() {
+        Object[] args = {
+                null,
+                KEY,
+                Faker.str_().random()
+        };
+        Result<UUID> result = new DefaultGetter<UUID>(KEY, DEFAUT_SUPPLIER).apply(args);
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.value()).isEqualTo(DEFAULT_VALUE);
+        assertThat(result.seed()).isNull();
+    }
+
+    @Test
+    void shouldCheckGetting() {
+        UUID expected = Faker.uuid_().random();
+        Object[] args = {
+                null,
+                KEY,
+                expected
+        };
+        Result<UUID> result = new DefaultGetter<UUID>(KEY, DEFAUT_SUPPLIER).apply(args);
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.value()).isEqualTo(expected);
+        assertThat(result.seed()).isNull();
+    }
+}
