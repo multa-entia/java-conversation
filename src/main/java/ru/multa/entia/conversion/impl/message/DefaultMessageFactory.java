dc36cfe097c15def35b222929aa5cd697555fe40
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-10-16 23:04:51 +0300

    DefaultMessageFactory is done

--- src/main/java/ru/multa/entia/conversion/impl/message/DefaultMessageFactory.java
+++ src/main/java/ru/multa/entia/conversion/impl/message/DefaultMessageFactory.java
@@ -7,10 +7,19 @@ import ru.multa.entia.conversion.api.SimpleFactory;
 import ru.multa.entia.conversion.api.address.Address;
 import ru.multa.entia.conversion.api.content.Content;
 import ru.multa.entia.conversion.api.message.Message;
+import ru.multa.entia.conversion.api.message.MessageCreator;
+import ru.multa.entia.conversion.impl.content.DefaultContentFactory;
+import ru.multa.entia.conversion.impl.getter.DefaultConditionGetter;
+import ru.multa.entia.conversion.impl.getter.DefaultValueGetter;
 import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.api.seed.Seed;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
+import ru.multa.entia.results.impl.seed.DefaultSeedBuilder;
 
+import java.util.Arrays;
 import java.util.UUID;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DefaultMessageFactory implements SimpleFactory<Object, Message> {
     @RequiredArgsConstructor
@@ -25,6 +34,15 @@ public class DefaultMessageFactory implements SimpleFactory<Object, Message> {
         private final String value;
     }
 
+    @RequiredArgsConstructor
+    @Getter
+    public enum Code {
+        FROM_ABSENCE("default-message-factory.from-absence"),
+        TO_ABSENCE("default-message-factory.to-absence");
+
+        private final String value;
+    }
+
     private final Checker<Object> checker;
     private final SimpleFactory<Object, Content> contentFactory;
     private final Function<Object[], Result<UUID>> idGetter;
@@ -32,9 +50,10 @@ public class DefaultMessageFactory implements SimpleFactory<Object, Message> {
     private final Function<Object[], Result<Address>> fromGetter;
     private final Function<Object[], Result<Address>> toGetter;
     private final Function<Object[], Result<Boolean>> confirmGetter;
+    private final MessageCreator creator;
 
     public DefaultMessageFactory() {
-        this(null, null, null, null, null, null, null);
+        this(null, null, null, null, null, null, null, null);
     }
 
     public DefaultMessageFactory(final Checker<Object> checker,
@@ -43,18 +62,80 @@ public class DefaultMessageFactory implements SimpleFactory<Object, Message> {
                                  final Function<Object[], Result<UUID>> conversationGetter,
                                  final Function<Object[], Result<Address>> fromGetter,
                                  final Function<Object[], Result<Address>> toGetter,
-                                 final Function<Object[], Result<Boolean>> confirmGetter) {
-        this.checker = checker;
-        this.contentFactory = contentFactory;
-        this.idGetter = idGetter;
-        this.conversationGetter = conversationGetter;
-        this.fromGetter = fromGetter;
-        this.toGetter = toGetter;
-        this.confirmGetter = confirmGetter;
+                                 final Function<Object[], Result<Boolean>> confirmGetter,
+                                 final MessageCreator creator) {
+        this.checker = checker == null ? new DefaultMessageChecker() : checker;
+        this.contentFactory = contentFactory == null ? new DefaultContentFactory() : contentFactory;
+        this.idGetter = idGetter == null ? new DefaultValueGetter<>(Key.ID, UUID::randomUUID) : idGetter;
+        this.conversationGetter = conversationGetter == null
+                ? new DefaultValueGetter<>(Key.CONVERSATION, UUID::randomUUID)
+                : conversationGetter;
+        this.fromGetter = fromGetter == null
+                ? new DefaultConditionGetter<>(Key.FROM, object -> {
+                    return object != null && Arrays.stream(object.getClass().getInterfaces()).collect(Collectors.toSet()).contains(Address.class)
+                            ? null
+                            : new DefaultSeedBuilder<Address>().code(Code.FROM_ABSENCE.getValue()).build();
+                })
+                : fromGetter;
+        this.toGetter = toGetter == null
+                ? new DefaultConditionGetter<>(Key.TO, object -> {
+                    return object != null && Arrays.stream(object.getClass().getInterfaces()).collect(Collectors.toSet()).contains(Address.class)
+                        ? null
+                        : new DefaultSeedBuilder<Address>().code(Code.TO_ABSENCE.getValue()).build();
+                })
+                : toGetter;
+        this.confirmGetter = confirmGetter == null
+                ? new DefaultValueGetter<>(Key.CONFIRM, () -> {return true;})
+                : confirmGetter;
+        this.creator = creator == null ? new DefaultMessageCreator() : creator;
     }
 
     @Override
     public Result<Message> create(final Object instance, final Object... args) {
-        return null;
+        Seed seed = checker.check(instance);
+        if (seed != null){
+            return DefaultResultBuilder.<Message>fail(seed);
+        }
+
+        Result<Content> contentResult = contentFactory.create(instance, args);
+        if (!contentResult.ok()){
+            return DefaultResultBuilder.<Message>fail(contentResult.seed());
+        }
+
+        Result<UUID> idResult = idGetter.apply(args);
+        if (!idResult.ok()){
+            return DefaultResultBuilder.<Message>fail(idResult.seed());
+        }
+
+        Result<UUID> conversationResult = conversationGetter.apply(args);
+        if (!conversationResult.ok()){
+            return DefaultResultBuilder.<Message>fail(conversationResult.seed());
+        }
+
+        Result<Address> fromResult = fromGetter.apply(args);
+        if (!fromResult.ok()){
+            return DefaultResultBuilder.<Message>fail(fromResult.seed());
+        }
+
+        Result<Address> toResult = toGetter.apply(args);
+        if (!toResult.ok()){
+            return DefaultResultBuilder.<Message>fail(toResult.seed());
+        }
+
+        Result<Boolean> confirmResult = confirmGetter.apply(args);
+        if (!confirmResult.ok()){
+            return DefaultResultBuilder.<Message>fail(confirmResult.seed());
+        }
+
+        return DefaultResultBuilder.<Message>ok(
+                creator.create(
+                        idResult.value(),
+                        conversationResult.value(),
+                        fromResult.value(),
+                        toResult.value(),
+                        confirmResult.value(),
+                        contentResult.value()
+                )
+        );
     }
 }
---
+++ src/test/java/ru/multa/entia/conversion/impl/message/DefaultMessageFactoryTest.java
@@ -0,0 +1,373 @@
+package ru.multa.entia.conversion.impl.message;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import ru.multa.entia.conversion.api.Checker;
+import ru.multa.entia.conversion.api.SimpleFactory;
+import ru.multa.entia.conversion.api.address.Address;
+import ru.multa.entia.conversion.api.content.Content;
+import ru.multa.entia.conversion.api.message.Message;
+import ru.multa.entia.fakers.impl.Faker;
+import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.api.seed.Seed;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
+import utils.TestAddress;
+import utils.TestContent;
+import utils.TestSeed;
+import utils.TestType;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DefaultMessageFactoryTest {
+    private static final Function<Seed, TestChecker> CHECKER_FUNC = seed -> {
+        TestChecker checker = Mockito.mock(TestChecker.class);
+        Mockito.when(checker.check(Mockito.any())).thenReturn(seed);
+
+        return checker;
+    };
+
+    private static final Function<Result<Content>, TestContentFactory> CONTENT_FACTORY_FUNC = result -> {
+        TestContentFactory factory = Mockito.mock(TestContentFactory.class);
+        Mockito.when(factory.create(Mockito.any(), Mockito.any())).thenReturn(result);
+
+        return factory;
+    };
+
+    private static final Function<Result<UUID>, TestUuidGetter> UUID_GETTER_FUNC = result -> {
+        TestUuidGetter getter = Mockito.mock(TestUuidGetter.class);
+        Mockito.when(getter.apply(Mockito.any())).thenReturn(result);
+
+        return getter;
+    };
+
+    private static final Function<Result<Address>, TestAddressGetter> ADDRESS_GETTER_FUNC = result -> {
+        TestAddressGetter getter = Mockito.mock(TestAddressGetter.class);
+        Mockito.when(getter.apply(Mockito.any())).thenReturn(result);
+
+        return getter;
+    };
+
+    private static final Function<Result<Boolean>, TestBooleanGetter> BOOLEAN_GETTER_FUNC = result -> {
+        TestBooleanGetter getter = Mockito.mock(TestBooleanGetter.class);
+        Mockito.when(getter.apply(Mockito.any())).thenReturn(result);
+
+        return getter;
+    };
+
+    @Test
+    void shouldCheckCreation_ifCheckerRetBadResult() {
+        String expectedCode = Faker.str_().random();
+        Result<Message> result = new DefaultMessageFactory(
+                CHECKER_FUNC.apply(new TestSeed(expectedCode, new Object[0])),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        ).create(null);
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(expectedCode);
+        assertThat(result.seed().args()).isEmpty();
+    }
+
+    @Test
+    void shouldCheckCreation_ifContentFactoryRetBadResult() {
+        String expectedCode = Faker.str_().random();
+        Result<Message> result = new DefaultMessageFactory(
+                CHECKER_FUNC.apply(null),
+                CONTENT_FACTORY_FUNC.apply(DefaultResultBuilder.<Content>fail(expectedCode)),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        ).create(null);
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(expectedCode);
+        assertThat(result.seed().args()).isEmpty();
+    }
+
+    @Test
+    void shouldCheckCreation_ifIdGetterRetBadResult() {
+        String expectedCode = Faker.str_().random();
+        TestContent content = new TestContent(new TestType(Faker.str_().random()), Faker.str_().random());
+        Result<Message> result = new DefaultMessageFactory(
+                CHECKER_FUNC.apply(null),
+                CONTENT_FACTORY_FUNC.apply(DefaultResultBuilder.<Content>ok(content)),
+                UUID_GETTER_FUNC.apply(DefaultResultBuilder.<UUID>fail(expectedCode)),
+                null,
+                null,
+                null,
+                null,
+                null
+        ).create(null);
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(expectedCode);
+        assertThat(result.seed().args()).isEmpty();
+    }
+
+    @Test
+    void shouldCheckCreation_ifConversationGetterRetBadResult() {
+        String expectedCode = Faker.str_().random();
+        TestContent content = new TestContent(new TestType(Faker.str_().random()), Faker.str_().random());
+        Result<Message> result = new DefaultMessageFactory(
+                CHECKER_FUNC.apply(null),
+                CONTENT_FACTORY_FUNC.apply(DefaultResultBuilder.<Content>ok(content)),
+                UUID_GETTER_FUNC.apply(DefaultResultBuilder.<UUID>ok(Faker.uuid_().random())),
+                UUID_GETTER_FUNC.apply(DefaultResultBuilder.<UUID>fail(expectedCode)),
+                null,
+                null,
+                null,
+                null
+        ).create(null);
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(expectedCode);
+        assertThat(result.seed().args()).isEmpty();
+    }
+
+    @Test
+    void shouldCheckCreation_ifFromGetterRetBadResult() {
+        String expectedCode = Faker.str_().random();
+        TestContent content = new TestContent(new TestType(Faker.str_().random()), Faker.str_().random());
+        Result<Message> result = new DefaultMessageFactory(
+                CHECKER_FUNC.apply(null),
+                CONTENT_FACTORY_FUNC.apply(DefaultResultBuilder.<Content>ok(content)),
+                UUID_GETTER_FUNC.apply(DefaultResultBuilder.<UUID>ok(Faker.uuid_().random())),
+                UUID_GETTER_FUNC.apply(DefaultResultBuilder.<UUID>ok(Faker.uuid_().random())),
+                ADDRESS_GETTER_FUNC.apply(DefaultResultBuilder.<Address>fail(expectedCode)),
+                null,
+                null,
+                null
+        ).create(null);
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(expectedCode);
+        assertThat(result.seed().args()).isEmpty();
+    }
+
+    @Test
+    void shouldCheckCreation_ifToGetterRetBadResult() {
+        String expectedCode = Faker.str_().random();
+        TestContent content = new TestContent(new TestType(Faker.str_().random()), Faker.str_().random());
+        Result<Message> result = new DefaultMessageFactory(
+                CHECKER_FUNC.apply(null),
+                CONTENT_FACTORY_FUNC.apply(DefaultResultBuilder.<Content>ok(content)),
+                UUID_GETTER_FUNC.apply(DefaultResultBuilder.<UUID>ok(Faker.uuid_().random())),
+                UUID_GETTER_FUNC.apply(DefaultResultBuilder.<UUID>ok(Faker.uuid_().random())),
+                ADDRESS_GETTER_FUNC.apply(DefaultResultBuilder.<Address>ok(new TestAddress(Faker.str_().random()))),
+                ADDRESS_GETTER_FUNC.apply(DefaultResultBuilder.<Address>fail(expectedCode)),
+                null,
+                null
+        ).create(null);
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(expectedCode);
+        assertThat(result.seed().args()).isEmpty();
+    }
+
+    @Test
+    void shouldCheckCreation_ifConfirmGetterRetBadResult() {
+        String expectedCode = Faker.str_().random();
+        TestContent content = new TestContent(new TestType(Faker.str_().random()), Faker.str_().random());
+        Result<Message> result = new DefaultMessageFactory(
+                CHECKER_FUNC.apply(null),
+                CONTENT_FACTORY_FUNC.apply(DefaultResultBuilder.<Content>ok(content)),
+                UUID_GETTER_FUNC.apply(DefaultResultBuilder.<UUID>ok(Faker.uuid_().random())),
+                UUID_GETTER_FUNC.apply(DefaultResultBuilder.<UUID>ok(Faker.uuid_().random())),
+                ADDRESS_GETTER_FUNC.apply(DefaultResultBuilder.<Address>ok(new TestAddress(Faker.str_().random()))),
+                ADDRESS_GETTER_FUNC.apply(DefaultResultBuilder.<Address>ok(new TestAddress(Faker.str_().random()))),
+                BOOLEAN_GETTER_FUNC.apply(DefaultResultBuilder.<Boolean>fail(expectedCode)),
+                null
+        ).create(null);
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(expectedCode);
+        assertThat(result.seed().args()).isEmpty();
+    }
+
+    @Test
+    void shouldCheckCreation() {
+        AtomicReference<Object> checkerInstanceHolder = new AtomicReference<>();
+        AtomicReference<Object> contentFactoryInstanceHolder = new AtomicReference<>();
+        AtomicReference<Object> contentFactoryArgHolder = new AtomicReference<>();
+        AtomicReference<Object> idGetterHolder = new AtomicReference<>();
+        AtomicReference<Object> conversationGetterHolder = new AtomicReference<>();
+        AtomicReference<Object> fromGetterHolder = new AtomicReference<>();
+        AtomicReference<Object> toGetterHolder = new AtomicReference<>();
+        AtomicReference<Object> confirmGetterHolder = new AtomicReference<>();
+
+        String instance = Faker.str_().random();
+        Long arg = Faker.long_().random();
+        Object[] args = {arg};
+
+        Supplier<TestChecker> checkerSup = () -> {
+            TestChecker checker = Mockito.mock(TestChecker.class);
+            Mockito
+                    .when(checker.check(Mockito.any()))
+                    .thenAnswer(new Answer<Seed>() {
+                        @Override
+                        public Seed answer(InvocationOnMock invocation) throws Throwable {
+                            checkerInstanceHolder.set(invocation.getArguments()[0]);
+                            return null;
+                        }
+                    });
+
+            return checker;
+        };
+
+        Function<Result<Content>, TestContentFactory> contentFactoryFunc = result -> {
+            TestContentFactory factory = Mockito.mock(TestContentFactory.class);
+            Mockito
+                    .when(factory.create(Mockito.any(), Mockito.any()))
+                    .thenAnswer(new Answer<Result<Content>>() {
+                        @Override
+                        public Result<Content> answer(InvocationOnMock invocation) throws Throwable {
+                            contentFactoryInstanceHolder.set(invocation.getArguments()[0]);
+                            contentFactoryArgHolder.set(invocation.getArguments()[1]);
+                            return result;
+                        }
+                    });
+
+            return factory;
+        };
+
+        Function<Result<UUID>, TestUuidGetter> idGetterFunc = result -> {
+            TestUuidGetter getter = Mockito.mock(TestUuidGetter.class);
+            Mockito
+                    .when(getter.apply(Mockito.any()))
+                    .thenAnswer(new Answer<Result<UUID>>() {
+                        @Override
+                        public Result<UUID> answer(InvocationOnMock invocation) throws Throwable {
+                            idGetterHolder.set(invocation.getArguments()[0]);
+                            return result;
+                        }
+                    });
+
+            return getter;
+        };
+
+        Function<Result<UUID>, TestUuidGetter> conversationGetterFunc = result -> {
+            TestUuidGetter getter = Mockito.mock(TestUuidGetter.class);
+            Mockito
+                    .when(getter.apply(Mockito.any()))
+                    .thenAnswer(new Answer<Result<UUID>>() {
+                        @Override
+                        public Result<UUID> answer(InvocationOnMock invocation) throws Throwable {
+                            conversationGetterHolder.set(invocation.getArguments()[0]);
+                            return result;
+                        }
+                    });
+
+            return getter;
+        };
+
+        Function<Result<Address>, TestAddressGetter> fromGetterFunc = result -> {
+            TestAddressGetter getter = Mockito.mock(TestAddressGetter.class);
+            Mockito
+                    .when(getter.apply(Mockito.any()))
+                    .thenAnswer(new Answer<Result<Address>>() {
+                        @Override
+                        public Result<Address> answer(InvocationOnMock invocation) throws Throwable {
+                            fromGetterHolder.set(invocation.getArguments()[0]);
+                            return result;
+                        }
+                    });
+
+            return getter;
+        };
+
+        Function<Result<Address>, TestAddressGetter> toGetterFunc = result -> {
+            TestAddressGetter getter = Mockito.mock(TestAddressGetter.class);
+            Mockito
+                    .when(getter.apply(Mockito.any()))
+                    .thenAnswer(new Answer<Result<Address>>() {
+                        @Override
+                        public Result<Address> answer(InvocationOnMock invocation) throws Throwable {
+                            toGetterHolder.set(invocation.getArguments()[0]);
+                            return result;
+                        }
+                    });
+
+            return getter;
+        };
+
+        Function<Result<Boolean>, TestBooleanGetter> confirmGetterFunc = result -> {
+            TestBooleanGetter getter = Mockito.mock(TestBooleanGetter.class);
+            Mockito
+                    .when(getter.apply(Mockito.any()))
+                    .thenAnswer(new Answer<Result<Boolean>>() {
+                        @Override
+                        public Result<Boolean> answer(InvocationOnMock invocation) throws Throwable {
+                            confirmGetterHolder.set(invocation.getArguments()[0]);
+                            return result;
+                        }
+                    });
+
+            return getter;
+        };
+
+        TestContent expectedContent = new TestContent(new TestType(Faker.str_().random()), Faker.str_().random());
+        UUID expectedId = Faker.uuid_().random();
+        UUID expectedConversation = Faker.uuid_().random();
+        TestAddress expectedFrom = new TestAddress(Faker.str_().random());
+        TestAddress expectedTo = new TestAddress(Faker.str_().random());
+        boolean expectedConfirm = true;
+        Result<Message> result = new DefaultMessageFactory(
+                checkerSup.get(),
+                contentFactoryFunc.apply(DefaultResultBuilder.<Content>ok(expectedContent)),
+                idGetterFunc.apply(DefaultResultBuilder.<UUID>ok(expectedId)),
+                conversationGetterFunc.apply(DefaultResultBuilder.<UUID>ok(expectedConversation)),
+                fromGetterFunc.apply(DefaultResultBuilder.<Address>ok(expectedFrom)),
+                toGetterFunc.apply(DefaultResultBuilder.<Address>ok(expectedTo)),
+                confirmGetterFunc.apply(DefaultResultBuilder.<Boolean>ok(expectedConfirm)),
+                new DefaultMessageCreator()
+        ).create(instance, arg);
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.seed()).isNull();
+
+        Message message = result.value();
+        assertThat(message.id()).isEqualTo(expectedId);
+        assertThat(message.conversation()).isEqualTo(expectedConversation);
+        assertThat(message.from()).isEqualTo(expectedFrom);
+        assertThat(message.to()).isEqualTo(expectedTo);
+        assertThat(message.confirm()).isEqualTo(expectedConfirm);
+        assertThat(message.content()).isEqualTo(expectedContent);
+
+        assertThat(checkerInstanceHolder.get()).isEqualTo(instance);
+        assertThat(contentFactoryInstanceHolder.get()).isEqualTo(instance);
+        assertThat(contentFactoryArgHolder.get()).isEqualTo(arg);
+        assertThat(idGetterHolder.get()).isEqualTo(args);
+        assertThat(conversationGetterHolder.get()).isEqualTo(args);
+        assertThat(fromGetterHolder.get()).isEqualTo(args);
+        assertThat(toGetterHolder.get()).isEqualTo(args);
+        assertThat(confirmGetterHolder.get()).isEqualTo(args);
+    }
+
+    private interface TestChecker extends Checker<Object> {}
+    private interface TestContentFactory extends SimpleFactory<Object, Content> {}
+    private interface TestUuidGetter extends Function<Object[], Result<UUID>> {}
+    private interface TestAddressGetter extends Function<Object[], Result<Address>> {}
+    private interface TestBooleanGetter extends Function<Object[], Result<Boolean>> {}
+}
