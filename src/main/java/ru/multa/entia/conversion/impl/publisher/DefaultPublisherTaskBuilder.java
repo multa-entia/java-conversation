22aa17fbbc0471d843d459d910032ea94d447cf4
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-10-28 18:22:54 +0300

    DefaultPublisherTaskBuilder is done

--- src/main/java/ru/multa/entia/conversion/impl/publisher/DefaultPublisherTaskBuilder.java
+++ src/main/java/ru/multa/entia/conversion/impl/publisher/DefaultPublisherTaskBuilder.java
@@ -1,5 +1,7 @@
 package ru.multa.entia.conversion.impl.publisher;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ru.multa.entia.conversion.api.ConversationItem;
 import ru.multa.entia.conversion.api.holder.HolderReleaseStrategy;
@@ -7,15 +9,41 @@ import ru.multa.entia.conversion.api.holder.HolderTimeoutStrategy;
 import ru.multa.entia.conversion.api.publisher.PublisherService;
 import ru.multa.entia.conversion.api.publisher.PublisherTask;
 import ru.multa.entia.conversion.api.publisher.PublisherTaskBuilder;
+import ru.multa.entia.conversion.api.publisher.PublisherTaskCreator;
 import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Slf4j
 class DefaultPublisherTaskBuilder<T extends ConversationItem> implements PublisherTaskBuilder<T> {
+    @RequiredArgsConstructor
+    @Getter
+    public enum Code {
+        SERVICE_IS_NULL("default-publisher-task-builder.service-is-null");
+
+        private final String value;
+    }
+
+    enum StrategiesUsage{
+        USE_SET,
+        USE_DEFAULT,
+        DO_NOT_USE
+    }
+
+    private final Map<StrategiesUsage, Function<DefaultPublisherTaskBuilder<T>, PublisherTask<T>>> creators = new HashMap<>(){{
+        put(StrategiesUsage.USE_SET, DefaultPublisherTaskBuilder::<T>createForUseSetStrategy);
+        put(StrategiesUsage.USE_DEFAULT, DefaultPublisherTaskBuilder::<T>createForUseDefaultStrategy);
+        put(StrategiesUsage.DO_NOT_USE, DefaultPublisherTaskBuilder::<T>createForDoNotUseStrategy);
+    }};
+
     private final Supplier<HolderTimeoutStrategy> timeoutStrategySup;
     private final Supplier<HolderReleaseStrategy> releaseStrategySup;
     private final PublisherService<T> service;
+    private final PublisherTaskCreator<T> creator;
 
     private StrategiesUsage strategiesUsage = StrategiesUsage.USE_SET;
     private HolderTimeoutStrategy timeoutStrategy;
@@ -24,15 +52,17 @@ class DefaultPublisherTaskBuilder<T extends ConversationItem> implements Publish
     private T item;
 
     public DefaultPublisherTaskBuilder() {
-        this(null, null, null);
+        this(null, null, null, null);
     }
 
     public DefaultPublisherTaskBuilder(final Supplier<HolderTimeoutStrategy> timeoutStrategySup,
                                        final Supplier<HolderReleaseStrategy> releaseStrategySup,
-                                       final PublisherService<T> service) {
+                                       final PublisherService<T> service,
+                                       final PublisherTaskCreator<T> creator) {
         this.timeoutStrategySup = timeoutStrategySup;
         this.releaseStrategySup = releaseStrategySup;
         this.service = service;
+        this.creator = creator == null ? new DefaultPublisherTaskCreator<>() : creator;
     }
 
     @Override
@@ -55,32 +85,43 @@ class DefaultPublisherTaskBuilder<T extends ConversationItem> implements Publish
 
     @Override
     public PublisherTaskBuilder<T> useSetStrategy() {
-        return null;
+        this.strategiesUsage = StrategiesUsage.USE_SET;
+        return this;
     }
 
     @Override
     public PublisherTaskBuilder<T> useDefaultStrategy() {
-        return null;
+        this.strategiesUsage = StrategiesUsage.USE_DEFAULT;
+        return this;
     }
 
     @Override
     public PublisherTaskBuilder<T> doNotUseStrategy() {
-        return null;
+        this.strategiesUsage = StrategiesUsage.DO_NOT_USE;
+        return this;
     }
 
     @Override
     public PublisherTask<T> build() {
-        return null;
+        return creators.get(strategiesUsage).apply(this);
     }
 
     @Override
     public Result<T> buildAndPublish() {
-        return null;
+        return service == null
+                ? DefaultResultBuilder.<T>fail(Code.SERVICE_IS_NULL.getValue())
+                : service.publish(build());
     }
 
-    enum StrategiesUsage{
-        USE_SET,
-        USE_DEFAULT,
-        DO_NOT_USE
+    private static <T extends ConversationItem> PublisherTask<T> createForDoNotUseStrategy(DefaultPublisherTaskBuilder<T> builder) {
+        return builder.creator.create(builder.item, null, null);
+    }
+
+    private static <T extends ConversationItem> PublisherTask<T> createForUseDefaultStrategy(DefaultPublisherTaskBuilder<T> builder) {
+        return builder.creator.create(builder.item, builder.timeoutStrategySup.get(), builder.releaseStrategySup.get());
+    }
+
+    private static <T extends ConversationItem> PublisherTask<T> createForUseSetStrategy(DefaultPublisherTaskBuilder<T> builder) {
+        return builder.creator.create(builder.item, builder.timeoutStrategy, builder.releaseStrategy);
     }
 }
--- src/test/java/ru/multa/entia/conversion/impl/publisher/DefaultPublisherTaskBuilderTest.java
+++ src/test/java/ru/multa/entia/conversion/impl/publisher/DefaultPublisherTaskBuilderTest.java
@@ -1,21 +1,38 @@
 package ru.multa.entia.conversion.impl.publisher;
 
 import lombok.SneakyThrows;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import ru.multa.entia.conversion.api.holder.HolderReleaseStrategy;
 import ru.multa.entia.conversion.api.holder.HolderTimeoutStrategy;
 import ru.multa.entia.conversion.api.message.Message;
+import ru.multa.entia.conversion.api.publisher.PublisherService;
+import ru.multa.entia.conversion.api.publisher.PublisherTask;
 import ru.multa.entia.conversion.api.publisher.PublisherTaskBuilder;
+import ru.multa.entia.conversion.api.publisher.PublisherTaskCreator;
+import ru.multa.entia.fakers.impl.Faker;
+import ru.multa.entia.results.api.result.Result;
+import ru.multa.entia.results.impl.result.DefaultResultBuilder;
 import utils.FakerUtil;
 import utils.TestHolderReleaseStrategy;
 import utils.TestHolderTimeoutStrategy;
 
 import java.lang.reflect.Field;
+import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultPublisherTaskBuilderTest {
+
+    private static final PublisherTaskCreator<Message> CREATOR = (item, timeoutStrategy, releaseStrategy) -> {
+        TestPublisherTask task = Mockito.mock(TestPublisherTask.class);
+        Mockito.when(task.item()).thenReturn(item);
+        Mockito.when(task.timeoutStrategy()).thenReturn(timeoutStrategy);
+        Mockito.when(task.releaseStrategy()).thenReturn(releaseStrategy);
+
+        return task;
+    };
+
     @SneakyThrows
     @Test
     void shouldCheckItemSetting() {
@@ -61,38 +78,204 @@ class DefaultPublisherTaskBuilderTest {
         assertThat(strategy).isEqualTo(expectedStrategy);
     }
 
+    @SneakyThrows
     @Test
-    void shouldCheckStrategiesUsage_doNotUseStrategy() {
+    void shouldCheckStrategiesUsage_strategy_doNotUse() {
+        DefaultPublisherTaskBuilder<Message> expectedBuilder = new DefaultPublisherTaskBuilder<>();
+        PublisherTaskBuilder<Message> builder = expectedBuilder.doNotUseStrategy();
 
+        assertThat(builder).isEqualTo(expectedBuilder);
+
+        Field field = builder.getClass().getDeclaredField("strategiesUsage");
+        field.setAccessible(true);
+        DefaultPublisherTaskBuilder.StrategiesUsage strategiesUsage
+                = (DefaultPublisherTaskBuilder.StrategiesUsage) field.get(builder);
+        assertThat(strategiesUsage).isEqualTo(DefaultPublisherTaskBuilder.StrategiesUsage.DO_NOT_USE);
+    }
+
+    @SneakyThrows
+    @Test
+    void shouldCheckStrategiesUsage_strategy_useDefault() {
+        DefaultPublisherTaskBuilder<Message> expectedBuilder = new DefaultPublisherTaskBuilder<>();
+        PublisherTaskBuilder<Message> builder = expectedBuilder.useDefaultStrategy();
+
+        assertThat(builder).isEqualTo(expectedBuilder);
+
+        Field field = builder.getClass().getDeclaredField("strategiesUsage");
+        field.setAccessible(true);
+        DefaultPublisherTaskBuilder.StrategiesUsage strategiesUsage
+                = (DefaultPublisherTaskBuilder.StrategiesUsage) field.get(builder);
+        assertThat(strategiesUsage).isEqualTo(DefaultPublisherTaskBuilder.StrategiesUsage.USE_DEFAULT);
+    }
+
+    @SneakyThrows
+    @Test
+    void shouldCheckStrategiesUsage_strategy_useSet() {
+        DefaultPublisherTaskBuilder<Message> expectedBuilder = new DefaultPublisherTaskBuilder<>();
+        PublisherTaskBuilder<Message> builder = expectedBuilder.useDefaultStrategy().useSetStrategy();
+
+        assertThat(builder).isEqualTo(expectedBuilder);
+
+        Field field = builder.getClass().getDeclaredField("strategiesUsage");
+        field.setAccessible(true);
+        DefaultPublisherTaskBuilder.StrategiesUsage strategiesUsage
+                = (DefaultPublisherTaskBuilder.StrategiesUsage) field.get(builder);
+        assertThat(strategiesUsage).isEqualTo(DefaultPublisherTaskBuilder.StrategiesUsage.USE_SET);
     }
 
     @Test
-    void shouldCheckStrategiesUsage_useDefaultStrategy() {
+    void shouldCheckBuilding_strategyNotSet() {
+        Message expectedItem = FakerUtil.randomMessage();
+        TestHolderTimeoutStrategy expectedTimeoutStrategy = new TestHolderTimeoutStrategy();
+        TestHolderReleaseStrategy expectedReleaseStrategy = new TestHolderReleaseStrategy();
+        PublisherTask<Message> task = new DefaultPublisherTaskBuilder<>(
+                () -> {return null;},
+                () -> {return null;},
+                null,
+                CREATOR
+        )
+                .item(expectedItem)
+                .timeoutStrategy(expectedTimeoutStrategy)
+                .releaseStrategy(expectedReleaseStrategy)
+                .build();
 
+        assertThat(task.item()).isEqualTo(expectedItem);
+        assertThat(task.timeoutStrategy()).isEqualTo(expectedTimeoutStrategy);
+        assertThat(task.releaseStrategy()).isEqualTo(expectedReleaseStrategy);
     }
 
     @Test
-    void shouldCheckStrategiesUsage_useSetStrategy() {
+    void shouldCheckBuilding_strategy_doNotUse() {
+        Message expectedItem = FakerUtil.randomMessage();
+        PublisherTask<Message> task = new DefaultPublisherTaskBuilder<>(
+                TestHolderTimeoutStrategy::new,
+                TestHolderReleaseStrategy::new,
+                null,
+                CREATOR
+        )
+                .item(expectedItem)
+                .doNotUseStrategy()
+                .build();
 
+        assertThat(task.item()).isEqualTo(expectedItem);
+        assertThat(task.timeoutStrategy()).isNull();
+        assertThat(task.releaseStrategy()).isNull();
     }
 
     @Test
-    void shouldCheckBuilding() {
+    void shouldCheckBuilding_strategy_useDefault() {
+        Message expectedItem = FakerUtil.randomMessage();
+        TestHolderTimeoutStrategy expectedTimeoutStrategy = new TestHolderTimeoutStrategy();
+        TestHolderReleaseStrategy expectedReleaseStrategy = new TestHolderReleaseStrategy();
+        PublisherTask<Message> task = new DefaultPublisherTaskBuilder<>(
+                () -> {return expectedTimeoutStrategy;},
+                () -> {return expectedReleaseStrategy;},
+                null,
+                CREATOR
+        )
+                .item(expectedItem)
+                .useDefaultStrategy()
+                .build();
 
+        assertThat(task.item()).isEqualTo(expectedItem);
+        assertThat(task.timeoutStrategy()).isEqualTo(expectedTimeoutStrategy);
+        assertThat(task.releaseStrategy()).isEqualTo(expectedReleaseStrategy);
+    }
+
+    @Test
+    void shouldCheckBuilding_strategy_useSet() {
+        Message expectedItem = FakerUtil.randomMessage();
+        TestHolderTimeoutStrategy expectedTimeoutStrategy = new TestHolderTimeoutStrategy();
+        TestHolderReleaseStrategy expectedReleaseStrategy = new TestHolderReleaseStrategy();
+        PublisherTask<Message> task = new DefaultPublisherTaskBuilder<>(
+                () -> {return null;},
+                () -> {return null;},
+                null,
+                CREATOR
+        )
+                .item(expectedItem)
+                .useSetStrategy()
+                .timeoutStrategy(expectedTimeoutStrategy)
+                .releaseStrategy(expectedReleaseStrategy)
+                .build();
+
+        assertThat(task.item()).isEqualTo(expectedItem);
+        assertThat(task.timeoutStrategy()).isEqualTo(expectedTimeoutStrategy);
+        assertThat(task.releaseStrategy()).isEqualTo(expectedReleaseStrategy);
     }
 
     @Test
     void shouldCheckBuildingAndPublishing_ifServiceIsNull() {
+        Message expectedItem = FakerUtil.randomMessage();
+        Result<Message> result = new DefaultPublisherTaskBuilder<>(
+                () -> {return null;},
+                () -> {return null;},
+                null,
+                CREATOR
+        )
+                .item(expectedItem)
+                .buildAndPublish();
 
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(DefaultPublisherTaskBuilder.Code.SERVICE_IS_NULL.getValue());
+        assertThat(result.seed().args()).isEmpty();
     }
 
     @Test
     void shouldCheckBuildingAndPublishing_ifServiceReturnsBadResult() {
+        String expectedCode = Faker.str_().random();
+        Supplier<TestPublisherService> supplier = () -> {
+            TestPublisherService service = Mockito.mock(TestPublisherService.class);
+            Mockito
+                    .when(service.publish(Mockito.any(TestPublisherTask.class)))
+                    .thenReturn(DefaultResultBuilder.<Message>fail(expectedCode));
 
+            return service;
+        };
+
+        Message expectedItem = FakerUtil.randomMessage();
+        Result<Message> result = new DefaultPublisherTaskBuilder<>(
+                () -> {return null;},
+                () -> {return null;},
+                supplier.get(),
+                CREATOR
+        )
+                .item(expectedItem)
+                .buildAndPublish();
+
+        assertThat(result.ok()).isFalse();
+        assertThat(result.value()).isNull();
+        assertThat(result.seed().code()).isEqualTo(expectedCode);
+        assertThat(result.seed().args()).isEmpty();
     }
 
     @Test
     void shouldCheckBuildingAndPublishing() {
+        Message expectedItem = FakerUtil.randomMessage();
+        Supplier<TestPublisherService> supplier = () -> {
+            TestPublisherService service = Mockito.mock(TestPublisherService.class);
+            Mockito
+                    .when(service.publish(Mockito.any(TestPublisherTask.class)))
+                    .thenReturn(DefaultResultBuilder.<Message>ok(expectedItem));
 
+            return service;
+        };
+
+        Result<Message> result = new DefaultPublisherTaskBuilder<>(
+                () -> {return null;},
+                () -> {return null;},
+                supplier.get(),
+                CREATOR
+        )
+                .item(expectedItem)
+                .buildAndPublish();
+
+        assertThat(result.ok()).isTrue();
+        assertThat(result.value()).isEqualTo(expectedItem);
+        assertThat(result.seed()).isNull();
     }
+
+    private interface TestPublisherTask extends PublisherTask<Message> {}
+    private interface TestPublisherService extends PublisherService<Message> {}
 }
