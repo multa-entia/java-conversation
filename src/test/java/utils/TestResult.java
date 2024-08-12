be53fb023550e1e8a720afb378368af4e35419e8
KasymbekovPN <kasymbekovPN@yandex.ru>
2024-08-12 22:55:36 +0300

    new version library are applied

--- src/test/java/utils/TestResult.java
+++ src/test/java/utils/TestResult.java
@@ -3,4 +3,6 @@ package utils;
 import ru.multa.entia.results.api.result.Result;
 import ru.multa.entia.results.api.seed.Seed;
 
-public record TestResult<T>(boolean ok, T value, Seed seed) implements Result<T> {}
+import java.util.List;
+
+public record TestResult<T>(boolean ok, T value, Seed seed, List<Result<?>> causes) implements Result<T> {}
